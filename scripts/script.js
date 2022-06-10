2777ffb1df8e9ff14807011df70a1f80448af9bd
Edward Savin <github@edwardcs.com>
2022-06-10 14:11:32 +0300

    Add operators functions and operate function

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+let a;
+let b;
+
+function add(a, b) {
+  let c = a + b;
+  return c;
+}
+
+function subtract(a, b) {
+  let c = a - b;
+  return c;
+}
+
+function multiply(a, b) {
+  let c = a * b;
+  return c;
+}
+
+function divide(a, b) {
+  let c = a / b;
+  return c;
+}
+
+function operate(a, x, b) {
+  switch (x) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      console.log("Invalid operator");
+  }
+}
